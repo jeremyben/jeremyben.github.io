39b2dd0ef19e38b3f8b816f5fc8166ba4008941f
jeremy <jerbens@gmail.com>
2015-03-03 23:44:30 +0100

    Ajout structure base avec bourbon, neat + svg inline

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ben.gg</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<?php include 'inline.php'; ?>
+
+<div class="content">
+
+
+
+    <div class="couille">
+        <p class="ui">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitat</p>
+    </div>
+
+</div>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/threejs/r69/three.min.js"></script> -->
+<script type="text/javascript" src="js/cool.js"></script>
+</body>
+</html>
