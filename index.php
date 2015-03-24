ce12debedf10bf0860fc4b795b8a8141995b2de0
jbensimon <bensimon.jeremy@gmail.com>
2015-03-25 00:22:19 +0100

    Réglages typo headings + expérimentation bourbon + réorganisation

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="format-detection" content="telephone=yes">
     <title>Ben.gg</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- add modernizer -->
 </head>
 <body>
 
@@ -13,10 +18,23 @@
 
 <div class="content">
 
+    <h1>Premier</h1>
+    <h2>Deuxième</h2>
+    <h3>Troisième</h3>
+    <h4>Quatrième</h4>
+    <h5>Cinquième</h5>
+    <h6>Sixième</h6>
+
+    <div class="intro">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore e cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+
     <div class="couille">
         <p class="ui">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
         quis nostrud exercitat</p>
+        <p class="lol">jlhs dlfd hlkk hfdls fg sdlkfhfd lhsdf sdfsdf uioqhldf apcncl ifjk dhjd j bj</p>
+        <p class="rofl">jlhs5 67854 5khfdls sdlkfsdf</p>
     </div>
 
 </div>
